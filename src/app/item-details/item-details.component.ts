1c2ee44ffed0a27020df336b8a202c149d86ba8e
Jonathan Sanchez <jonathanlsanch@gmail.com>
2018-05-20 17:14:03 -0400

    view details working

--- src/app/item-details/item-details.component.ts
+++ src/app/item-details/item-details.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ItemService } from './../item.service';
 
 @Component({
   selector: 'app-item-details',
   templateUrl: './item-details.component.html',
-  styleUrls: ['./item-details.component.css']
+  styleUrls: ['./item-details.component.css'],
+  providers: [ItemService]
 })
-export class ItemDetailsComponent implements OnInit {
 
-  constructor() { }
+export class ItemDetailsComponent implements OnInit {
+  item: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private itemService: ItemService
+  ) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.getItemDetails(params['id']);
+    });
   }
 
+  getItemDetails(id) {
+    this.itemService.get(id)
+      .subscribe((item) => {
+        this.item = item;
+      });
+  }
 }
