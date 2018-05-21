74476937d355ade601516562a9f7269380ae6d1c
Jonathan Sanchez <jonathanlsanch@gmail.com>
2018-05-21 12:35:27 -0400

    fix services for database

--- src/app/item.service.ts
+++ src/app/item.service.ts
@@ -2,34 +2,36 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import { environment } from '.././environments/environment';
 
 @Injectable()
 export class ItemService {
-  BASE_URL: string = 'http://localhost:3000';
+  // BASE_URL: string = 'http://localhost:3000';
+
   constructor(private http: Http) {}
 
   getList() {
-    return this.http.get(`${this.BASE_URL}/api/items`)
+    return this.http.get(`${environment.apiBase}/api/items`)
       .map((res) => res.json());
   }
 
   get(id) {
-    return this.http.get(`${this.BASE_URL}/api/items/${id}`)
+    return this.http.get(`${environment.apiBase}/api/items/${id}`)
       .map((res) => res.json());
   }
 
   newItem(dataToSend) {
-    return this.http.post(`${this.BASE_URL}/api/items`, dataToSend)
+    return this.http.post(`${environment.apiBase}/api/items`, dataToSend)
       .map((res) => res.json());
   }
 
   editItem(id, updates) {
-    return this.http.put(`${this.BASE_URL}/api/items/${id}`, updates)
+    return this.http.put(`${environment.apiBase}/api/items/${id}`, updates)
       .map((res) => res.json());
   }
 
   remove(id) {
-    return this.http.delete(`${this.BASE_URL}/api/items/${id}`)
+    return this.http.delete(`${environment.apiBase}/api/items/${id}`)
       .map((res) => res.json());
   }
 }
